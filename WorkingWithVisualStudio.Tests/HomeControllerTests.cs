1161e491230e3a4cd5ac888d6be4bb2c29f4ef32
robyoung91 <robertmatthewyoung@gmail.com>
2020-04-08 14:49:22 +0100

    Added Moq functionality

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -4,62 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 using WorkingWithVisualStudio.Controllers;
 using WorkingWithVisualStudio.Models;
 using Xunit;
+using Moq;
 
 namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
-    {
-        class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; } = new Product[] {
-                 new Product { Name = "P1", Price = 275M },
-                 new Product { Name = "P2", Price = 48.95M },
-                 new Product { Name = "P3", Price = 19.50M },
-                 new Product { Name = "P3", Price = 34.95M }
-            };
-
-            public void AddProduct(Product p)
-            {
-                // do nothing - not required for test
-            }
-        }
-
-        [Fact]
-        public void IndexActionMethodIsComplete()
+    {        
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
         {
             // Arrange
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository();
-
-            // Act
-            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
-
-            // Assert
-            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
-
-        }
-
-        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
-        {
-            public IEnumerable<Product> Products { get; } = new Product[] {
-                 new Product { Name = "P1", Price = 5M },
-                 new Product { Name = "P2", Price = 48.95M },
-                 new Product { Name = "P3", Price = 19.50M },
-                 new Product { Name = "P3", Price = 34.95M }
-            };
-
-            public void AddProduct(Product p)
-            {
-                // do nothing - not required for test
-            }
-        }
-
-        [Fact]
-        public void IndexActionModelIsCompletePricesUnder50()
-        {
-            // Arrange
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController { Repository = mock.Object };
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model
@@ -69,6 +27,22 @@ namespace WorkingWithVisualStudio.Tests
             Assert.Equal(controller.Repository.Products, model,
             Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
             && p1.Price == p2.Price));
+        }
+
+        [Fact]
+        public void RepositoryPropertyCalledOnce()
+        {
+            // Arrange
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products)
+                .Returns(new[] { new Product { Name = "P1", Price = 100 } });
+            var controller = new HomeController { Repository = mock.Object };
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
